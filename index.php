25412a8d76e462dab13c1e0f8b160834daa442b6
A-Kaiyum <akaiyum.cse@gmail.com>
2021-09-07 23:38:56 +0600

    All about of variable practice done

--- index.php
+++ index.php
@@ -9,11 +9,39 @@
 </head>
 <body>
 <?php
-// single line comment
-#single line comment
-/*
- multiline comment
- */
+// what is a variable
+// variable is a container which is store values
+// Declare variable
+$name = 'mac';
+$age = 28;
+$isMale = true;
+$height = 1.85;
+$salary = null;
+
+// Print the variable
+echo $name.'<br>';
+echo $age.'<br>';
+echo $isMale.'<br>';
+echo $height.'<br>';
+echo $salary.'<br>';
+//Print the whole variable
+var_dump($name,$age,$isMale,$height,$salary);
+// change the value of variable
+$name = false;
+// Print type of variable
+echo gettype($name);
+// Variable checking function
+is_string($name);
+is_int($age);
+//checking variable is define
+isset($name);
+isset($address);
+// Define constant
+define('PI',3.1416);
+echo PI;
+// Using PHP Built in function
+echo SORT_ASC;
+echo PHP_INT_MAX;
 ?>
 </body>
 </html>
